76f011c35e1ccfea68e5a2a865d9da98a2993873
Symplectic Stoat <67303960+SymplecticStoat@users.noreply.github.com>
2026-04-30 16:06:05 -0700

    Does adding a file of the same name overwrite?

--- Pluto_Test.jl
+++ Pluto_Test.jl
@@ -7,6 +7,17 @@ using InteractiveUtils
 # ╔═╡ 62a2ffc0-44bc-11f1-88b2-8329525a1951
 1+1
 
+# ╔═╡ d092f5cf-8c8a-47b6-8fbf-ef85bccea241
+md"""
+Some markdown code: $a^2 + b^2 = c^2$. <br>
+cmd + 'M' converts the whole cell into Markdown! <br>
+While in a cell, cntrl + enter makes a new cell below. 
+
+"""
+
+# ╔═╡ 7875816e-f805-45cb-b50b-5e109bad6b8b
+
+
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
 [deps]
@@ -25,5 +36,7 @@ project_hash = "71853c6197a6a7f222db0f1978c7cb232b87c5ee"
 
 # ╔═╡ Cell order:
 # ╠═62a2ffc0-44bc-11f1-88b2-8329525a1951
+# ╟─d092f5cf-8c8a-47b6-8fbf-ef85bccea241
+# ╠═7875816e-f805-45cb-b50b-5e109bad6b8b
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
