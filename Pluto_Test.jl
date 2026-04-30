970626e7b2a6e4091de94c3add934ce8a1da9138
SymplecticStoat <67303960+SymplecticStoat@users.noreply.github.com>
2026-04-30 23:07:23 +0000

    Deploying to gh-pages from @ SymplecticStoat/DFSOS_verification@76f011c35e1ccfea68e5a2a865d9da98a2993873 🚀

---
+++ Pluto_Test.jl
@@ -0,0 +1,42 @@
+### A Pluto.jl notebook ###
+# v0.20.24
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 62a2ffc0-44bc-11f1-88b2-8329525a1951
+1+1
+
+# ╔═╡ d092f5cf-8c8a-47b6-8fbf-ef85bccea241
+md"""
+Some markdown code: $a^2 + b^2 = c^2$. <br>
+cmd + 'M' converts the whole cell into Markdown! <br>
+While in a cell, cntrl + enter makes a new cell below. 
+
+"""
+
+# ╔═╡ 7875816e-f805-45cb-b50b-5e109bad6b8b
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.12.6"
+manifest_format = "2.0"
+project_hash = "71853c6197a6a7f222db0f1978c7cb232b87c5ee"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═62a2ffc0-44bc-11f1-88b2-8329525a1951
+# ╟─d092f5cf-8c8a-47b6-8fbf-ef85bccea241
+# ╠═7875816e-f805-45cb-b50b-5e109bad6b8b
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
